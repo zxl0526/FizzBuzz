fd88662963070cf8b26ed59ca109d7dac988701b
zxl0526 <851450219@qq.com>
2020-06-15 16:53:12 +0800

    20200615_3&&5&&7条件判断_zxl

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -6,18 +6,27 @@ package com.thoughtworks;
  */
 public class FizzBuzz {
     public String say(int number) {
+        String fizz="Fizz";
+        String buzz="Buzz";
+        String whizz="Whizz";
         int three = 3;
         int five = 5;
-        if(number % three ==0 && number % five != 0 ){
-            return "Fizz";
+        int seven = 7;
+        if(number % three ==0 && number % five != 0 && number % seven != 0 ){
+            return fizz;
         }
         if(number % five == 0  && number % three != 0){
-            return "Buzz";
+            return buzz;
         }
         if(number % three ==0 && number % five == 0){
-            return "FizzBuzz";
+            return fizz+buzz;
         }
-
+        if(number % seven ==0 && number % three !=0 ){
+            return whizz;
+        }
+        if(number % seven ==0 && number % three ==0 ){
+            return fizz+whizz;
+        }
         return String.valueOf(number);
     }
 }
--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -4,56 +4,35 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class FizzBuzzTest {
+
     @Test
-    public void  should_return_1_when_given_1(){
-
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String result = fizzBuzz.say(1);
-
-        // then
-        assertEquals("1",result);
-
-    }
+    public void  should_return_String_when_given_1(){
+        should_return_String_when_given_number(1,"1");
+    };
     @Test
-    public void  should_return_Fizz_when_given_3(){
-
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String result = fizzBuzz.say(3);
-
-        // then
-        assertEquals("Fizz",result);
-
-    }
+    public void  should_return_String_when_given_3(){
+        should_return_String_when_given_number(3,"Fizz");
+    };
     @Test
-    public void  should_return_Buzz_when_given_5(){
-
-        // given
-        FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String result = fizzBuzz.say(5);
-
-        // then
-        assertEquals("Buzz",result);
-
-    }
+    public void  should_return_String_when_given_5(){
+        should_return_String_when_given_number(5,"Buzz");
+    };
     @Test
-    public void  should_return_FizzBuzz_when_given_15(){
+    public void  should_return_String_when_given_15(){
+        should_return_String_when_given_number(15,"FizzBuzz");
+    };
+    @Test
+    public void  should_return_String_when_given_7(){
+        should_return_String_when_given_number(7,"Whizz");
+    };
+    @Test
+    public void  should_return_String_when_given_21(){
+        should_return_String_when_given_number(21,"FizzWhizz");
+    };
+    public void  should_return_String_when_given_number(int num,String resultA){
 
-        // given
         FizzBuzz fizzBuzz = new FizzBuzz();
-
-        // when
-        String result = fizzBuzz.say(15);
-
-        // then
-        assertEquals("FizzBuzz",result);
-
+        String result = fizzBuzz.say(num);
+        assertEquals(resultA,result);
     }
 }
